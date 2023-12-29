e357d9a893055e28d76b11e0e36d68beb703301d
tshiyuoo <tshiyuoo@gmail.com>
2023-12-29 17:30:43 +0800

    去掉首页横幅

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -128,7 +128,7 @@ const LayoutIndex = props => {
     <LayoutBase {...props} slotRight={slotRight} headerSlot={headerSlot}>
       <div id="post-outer-wrapper" className="px-5 md:px-0">
         {/* 文章分类条 */}
-        <CategoryBar {...props} />
+        {/* <CategoryBar {...props} /> */}
         {siteConfig('POST_LIST_STYLE') === 'page'
           ? (
           <BlogPostListPage {...props} />
