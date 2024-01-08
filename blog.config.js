b82134dbbdfa6b957124be6c7e2fbc29f2d7d0ea
tshiyuoo <tshiyuoo@gmail.com>
2024-01-08 12:16:28 +0800

    设置网站地址为空

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'tshiyuoo', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '珍爱生命，远离IT', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tshiyuoo.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'tshiyuoo', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
